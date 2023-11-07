cbdec0078e6e8f85722cb4f0559ef691c6073cb7
IsaBec <146860107+IsaBec@users.noreply.github.com>
2023-11-07 17:32:10 +0100

    Update 04.remotesensing.visualisation.R

--- 04.remotesensing.visualisation.R
+++ 04.remotesensing.visualisation.R
@@ -42,3 +42,54 @@ plot(b2, col=clb)
 b3 <- im.import("sentinel.dolomites.b3.tif") # b2 is the green wavelength
 b3
 plot(b3, col=clb)
+
+
+######
+class missed
+
+
+
+
+#####
+##library(imageRy)
+##library(terra)   to load again
+# importing data
+# blue band
+b2 <- im.import("sentinel.dolomites.b2.tif") # b2 is the blue wavelength
+b2
+
+# green band
+b3 <- im.import("sentinel.dolomites.b3.tif") # b3 is the green wavelength
+b3
+
+# red band
+b4 <- im.import("sentinel.dolomites.b4.tif") # b4 is the red wavelength
+b4
+
+# NIR band
+b8 <- im.import("sentinel.dolomites.b8.tif") # b8 is the NIR wavelength
+b8
+
+###stack images
+
+stacksent <- c(b2, b3, b4, b8)
+dev.off() # it closes devices
+plot(stacksent, col=cl)
+
+# RGB space--- color venn diagram for red green blue (rgb) 
+# stacksent: 
+# band2 blue element 1, stacksent[[1]] 
+# band3 green element 2, stacksent[[2]]
+# band4 red element 3, stacksent[[3]]
+# band8 nir element 4, stacksent[[4]]
+im.plotRGB(stacksent, r=3, g=2, b=1)        ##r,g,b colors
+im.plotRGB(stacksent, r=4, g=3, b=2)         ##everyhting red is vegetation, reflectance (near infrared on top of red)
+
+im.plotRGB(stacksent, r=3, g=4, b=2)        ##everything green is vegetation, bare soil, roads etc will be violet
+im.plotRGB(stacksent, r=3, g=2, b=4)        ## blue veg reflecting IR (bc more info by infrared)
+
+
+##see if info is correlated with each other (bands) see how pixels relate to each other
+##pairs - scatterplot matrices 
+pairs(stacksent)        ## its gonna be 6 4x3)/2 idk 
+
