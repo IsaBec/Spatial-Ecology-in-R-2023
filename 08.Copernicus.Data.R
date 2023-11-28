49ad92076b2649b5b16ba6e7fac4921ac6960ea2
IsaBec <146860107+IsaBec@users.noreply.github.com>
2023-11-28 20:50:17 +0100

    Update 08.Copernicus.Data.R

--- 08.Copernicus.Data.R
+++ 08.Copernicus.Data.R
@@ -22,3 +22,23 @@ plot(vegpropertiesc, col=cl)
 ##crop in the same place and compare
 
 
+###Class' example
+soilm2023 <- rast("c_gls_SSM1km_202311250000_CEURO_S1CSAR_V1.2.1.nc")
+plot(soilm2023)
+
+# there are two elements, let's use the first one!
+plot(soilm2023[[1]])
+
+cl <- colorRampPalette(c("red", "orange", "yellow")) (100)
+plot(soilm2023[[1]], col=cl)
+
+ext <- c(22, 26, 55, 57) # minlong, maxlong, minlat, maxlat
+soilm2023c <- crop(soilm2023, ext)
+
+plot(soilm2023c[[1]], col=cl)
+
+# new image
+soilm2023_24 <- rast("c_gls_SSM1km_202311240000_CEURO_S1CSAR_V1.2.1.nc")
+plot(soilm2023_24)
+soilm2023_24c <- crop(soilm2023_24, ext)
+plot(soilm2023_24c[[1]], col=cl)
