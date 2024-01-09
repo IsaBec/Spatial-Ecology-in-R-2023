3ab136c37cff5471e1866a9178e579c841062f02
IsaBec <146860107+IsaBec@users.noreply.github.com>
2024-01-09 18:58:23 +0100

    Create 12.RMarkDown.R

---
+++ 12.RMarkDown.R
@@ -0,0 +1,21 @@
+---
+title: "Markdown trial 1"
+author: "Isabel Becdach"
+date: "1/9/2024"
+output: html_document
+---
+
+# My First markdown document
+
+Here you can add some text if you want
+
+In this case you obtain a results
+```{r, eval=T}  
+2+3
+```
+
+In this case you do not:
+```{r, eval=F}  
+2+3
+```
+##eval-if code should lead to result or not (cant include comments)
