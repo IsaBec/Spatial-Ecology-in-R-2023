0656fb1c79ade5623e60c6480722c99e9656dcf3
IsaBec <146860107+IsaBec@users.noreply.github.com>
2025-08-29 14:20:10 +0200

    Update 08.Copernicus.Data.R

--- 08.Copernicus.Data.R
+++ 08.Copernicus.Data.R
@@ -5,6 +5,39 @@ install.packages("ncdf4")
 library(ncdf4)
 library(terra)
 
+#in COPERNICUS-- there are four main variables:VEGETATION, ENERGY,WATER CYCLE AND CRYOSPHERE
+
+###VEGETATION:
+#FAPAR -> fraction of absorbed photosynthetically active radiation. 
+          #Related to the red band (used by plants for photosynthesis)
+#NDVI -> Normalized difference vegetation index
+#VCI -> vegetation condition index. it compares the current NDVI to the range of 
+          #values observed during the same period in previous years
+#VPI -> vegetation productivity index. it assess the vegetation condition by referencing the
+          #current value of the NDVI with the long-term statistics for the same period.
+#dry matter productivity -> represents the overall growth rate
+#burnt area -> maps the burnt scars over the planet.
+#surface soil moisture
+#soil water index -> is the relative water content of the top few centimeters of soil.
+
+
+###ENERGY:
+#top of canopy reflectance -> the spectral reflectance by the fraction at the top of vegetation, soil, houses (or whatever)
+#surface albedo 
+#land surface temperature -> the radiative skin surface temperature of land
+
+
+###WATER CYCLE
+#water temperatures of lakes LSWT 
+#lake water quality -> monitoring water quality in lakes and reservoirs is key in maintaining safe water ...
+#estimate of water bodies present in the whole planet
+#water level
+
+###CRYOSPHERE
+#Snow cover -> estimate of the amount of snow in a certain area
+#lake ice extent
+#snow water equivalence -> passage from solid (snow) to liquid (water) phase
+
 setwd("/Users/alvarobecdach/Downloads")
 vegproperties <- rast("c_gls_LAI_202006130000_GLOBE_PROBAV_V1.5.1.nc")  ##put .nc or whatever the file ending
 
@@ -14,6 +47,7 @@ plot(vegproperties[[1]])  ##get 1st image
 cl <- colorRampPalette(c("red","orange","yellow"))(100)
 plot(vegproperties[[1]], col=cl)
 
+#using the function "crop()" to pick only a single region of our interest
 ext <- c(10000, 13000, 3000,5000) ##min longitud, max longitud, min lat, max lat
 vegpropertiesc <-crop(vegproperties[[1]], ext)
 plot(vegpropertiesc, col=cl)
